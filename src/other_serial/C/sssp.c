da5999974bbf51bf6bf4ee93598ffbc69263ba2f
P.R. Merkey <prmerkey@gmail.com>
2020-09-29 12:22:13 -0400

    Pulled in the changes from new_c_argp

--- src/other_serial/C/sssp.c
+++ src/other_serial/C/sssp.c
@@ -1,48 +1,23 @@
-/******************************************************************
-//
-//
-// Copyright(C) 2018, Institute for Defense Analyses
-// 4850 Mark Center Drive, Alexandria, VA; 703-845-2500
-// This material may be reproduced by or for the US Government
-// pursuant to the copyright license under the clauses at DFARS
-// 252.227-7013 and 252.227-7014.
-// 
-//
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//  * Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-//  * Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the distribution.
-//  * Neither the name of the copyright holder nor the
-//   names of its contributors may be used to endorse or promote products
-//   derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-// COPYRIGHT HOLDER NOR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
-*****************************************************************/ 
+/*******************************************************************/
+/* Copyright (c) 2020, Institute for Defense Analyses              */
+/* 4850 Mark Center Drive, Alexandria, VA 22311-1882; 703-845-2500 */
+/*                                                                 */
+/* All rights reserved.                                            */
+/*                                                                 */
+/* This file is part of Bale.   For licence information see the    */
+/* LICENSE file in the top level dirctory of the distribution.     */
+/*******************************************************************/
 
 /*! \file sssp.c
- * \brief Driver program that calls different 
- * Single Source Shortest Path alogrithms.
+ * \brief The application that calls different Single Source Shortest Path alogrithms.
+ *
+ * It calls several versions of Dijsktra's algorithm, the Bellman Ford algorithm, and
+ * the Delta-stepping algorithm.
  */
 
 #include "spmat_utils.h"
 #include "std_options.h"
+#include "default_app_sizes.h"
 
 double sssp_dijsktra_linear(d_array_t * tent, sparsemat_t * mat, int64_t v0);
 double sssp_dijsktra_heap(d_array_t * tent, sparsemat_t * mat, int64_t r0);
@@ -70,6 +45,7 @@ static double sssp_answer_diff(d_array_t *A, d_array_t *B)
   return(sqrt(diff));
 }
 
+/********************************  argp setup  ************************************/
 typedef struct args_t{
   double deltaStep; 
   int64_t V0;
@@ -78,7 +54,8 @@ typedef struct args_t{
   std_graph_args_t gstd;
 }args_t;
 
-static int parse_opt(int key, char * arg, struct argp_state * state){
+static int parse_opt(int key, char * arg, struct argp_state * state)
+{
   args_t * args = (args_t *)state->input;
   switch(key)
     {
@@ -114,78 +91,43 @@ static struct argp_child children_parsers[] =
 
 int main(int argc, char * argv[]) 
 {
-  args_t args ={0};  
-  struct argp argp = {options, parse_opt, 0, "C version of Single Source Shortest Path algorithms.", children_parsers};
+  enum MODEL {GENERIC_Model=1, DIJSKTRA_HEAP=2, DELTA_STEPPING=4, BELLMAN_SIMPLE=8, BELLMAN=16, ALL_Models=32};
+  args_t args={0};  
+  args.std.models_mask = ALL_Models - 1;
+  struct argp argp = {options, parse_opt, 0, "SSSP for a weighted graph.", children_parsers};
   argp_parse(&argp, argc, argv, 0, 0, &args);
+  args.gstd.numrows = SSSP_NUM_ROWS;
+  int ret = bale_app_init(argc, argv, &args, sizeof(args_t), &argp, &args.std);
+  if (ret < 0) return(ret);
+  else if (ret) return(0);
+
+  //override command line 
+  if ((args.gstd.loops == LOOPS) || (args.gstd.directed == 0) || (args.gstd.weighted == 0)) {
+    args.gstd.loops = NOLOOPS;
+    args.gstd.directed = 1;
+    args.gstd.weighted = 1;
+  }
+
+  write_std_graph_options(&args.std, &args.gstd);
+  write_std_options(&args.std);
   
-  double nz_per_row = args.gstd.nz_per_row;
-  double edge_prob = args.gstd.edge_prob;
-  int64_t numrows = args.gstd.numrows;
-  edge_type edge_type = UNDIRECTED;
-  self_loops loops = LOOPS;
-  int quiet = args.std.quiet;
+  // read in a matrix or generate a random graph
+  sparsemat_t * mat = get_input_graph(&args.std, &args.gstd);
+  if(!mat){fprintf(stderr, "ERROR: SSSP: mat is NULL!\n");return(-1);}
 
-  graph_model model = args.gstd.model;
-  
-  if(args.gstd.readfile == 0){
-    resolve_edge_prob_and_nz_per_row(&edge_prob, &nz_per_row, numrows, edge_type, loops);
-  }
-  
-#if 0
-  if(!quiet ) {
-    fprintf(stderr,"Running C versions of SSSP\n");
-    if(args.gstd.readfile == 1)
-      fprintf(stderr,"Reading a matrix from file (-f [%s])\n", args.gstd.filename);
-    else{
-      if(args.gstd.model == FLAT)
-        fprintf(stderr,"flat model           (-F)\n");
-      else        
-        fprintf(stderr,"geometric model      (-G)\n");
-      fprintf(stderr,"Number of rows       (-n) %"PRId64"\n", numrows);
-      fprintf(stderr,"edge_density         (-e)= %lg\n", edge_prob);
-      fprintf(stderr,"nz_per_row           (-z)= %lg\n", nz_per_row);
-      fprintf(stderr,"random seed          (-s)= %ld\n",  args.std.seed);
-    }
-    fprintf(stderr,"models_mask          (-M)= %d\n", args.std.models_mask);
-    fprintf(stderr,"dump_files           (-D)= %d\n", args.std.dump_files);
-    fprintf(stderr,"---------------------------------------\n");
-  }
-#endif
-
-  sparsemat_t *mat;
-  if(args.gstd.readfile) {
-    mat = read_matrix_mm(args.gstd.filename);
-    if(!mat){printf("ERROR: sssp: read graph from %s Failed\n", args.gstd.filename); exit(1);}
-  } else {
-    mat = random_graph(numrows, model, DIRECTED_WEIGHTED, NOLOOPS, edge_prob, args.std.seed);
-    if(!mat){ printf("ERROR: sssp: erdos_renyi_graph Failed\n"); exit(1); }
-  }
-
-  if(!quiet){
-    printf("Input matrix stats:\n");
-    spmat_stats(mat);
-    fprintf(stderr,"---------------------------------------\n");
-  }
-
-  if(args.std.dump_files){
-    dump_matrix(mat, 20, "mat.out");
-    write_matrix_mm(mat, "ssspout.mm");
-  }
+  if(args.std.dump_files) write_matrix_mm(mat, "sssp_inmat");
 
   uint32_t use_model;
-  enum MODEL {GENERIC_Model=1, DIJSKTRA_HEAP=2, DELTA_STEPPING=4, BELLMAN_SIMPLE=8, BELLMAN=16, ALL_Models=32};
-  int64_t models_mask = args.std.models_mask;
-  models_mask=ALL_Models - 1;
  
   double laptime = 0.0;
   char model_str[32];
 
   d_array_t *comp_tent=NULL;
-  d_array_t *tent = init_d_array(numrows);
+  d_array_t *tent = init_d_array(mat->numrows);
 
   for(use_model=1; use_model < ALL_Models; use_model *=2 ){
     model_str[0] = '\0';
-    switch( use_model & models_mask ){
+    switch( use_model & args.std.models_mask ){
     case GENERIC_Model:
       sprintf(model_str, "Dijsktra Linear");
       set_d_array(tent, INFINITY);
--- src/other_serial/C/std_options.h
+++ src/other_serial/C/std_options.h
@@ -1,26 +1,58 @@
+/*******************************************************************/
+/* Copyright (c) 2020, Institute for Defense Analyses              */
+/* 4850 Mark Center Drive, Alexandria, VA 22311-1882; 703-845-2500 */
+/*                                                                 */
+/* All rights reserved.                                            */
+/*                                                                 */
+/* This file is part of Bale.   For licence information see the    */
+/* LICENSE file in the top level dirctory of the distribution.     */
+/*******************************************************************/
+
 #ifndef STD_OPTIONS_H
 #define STD_OPTIONS_H
-#include <argp.h>
+
 #include <stdlib.h>
 #include <stdint.h>
 #include "spmat_utils.h"
+#include <argp.h>
+
+#define C_BALE_VERSION 3.0
 
 typedef struct std_args_t{
   int dump_files;
+  int json;
+  char json_output[128];
   int models_mask;
   int quiet;
   int64_t seed;
 }std_args_t;
 
+extern struct argp std_options_argp;
 typedef struct std_graph_args_t{
   int64_t numrows;
   int readfile;  
-  char * filename;
+  char filename[128];
   graph_model model;
   double edge_prob;
+  int directed;
+  int loops;
+  int weighted;
   double nz_per_row;
+  char kron_string[128];
+  int kron_spec[64];
+  int kron_num;
+  int kron_mode;
 } std_graph_args_t;
 
-extern struct argp std_options_argp;
 extern struct argp std_graph_options_argp;
+
+sparsemat_t *  get_input_graph(std_args_t * sargs, std_graph_args_t * gargs);       // TODO should this be in spmat_utils
+void           write_std_options(std_args_t * sargs);
+void           write_std_graph_options(std_args_t * sargs, std_graph_args_t * gargs);
+
+int  bale_app_init(int argc, char ** argv, void * args, int arg_len, struct argp * argp, std_args_t * sargs);
+void bale_app_finish(std_args_t * sargs);
+void bale_app_write_int(std_args_t * sargs, char * key, int64_t val);
+void bale_app_write_time(std_args_t * sargs, char * model_str, double time);
+
 #endif
