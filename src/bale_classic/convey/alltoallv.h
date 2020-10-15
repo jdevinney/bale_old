77cd1fd36f247e4841d63d59046bb3412c5f218d
F. Miller Maley <fmmaley@gmail.com>
2020-10-15 12:32:20 -0400

    Update copyright and license notices

--- src/bale_classic/convey/alltoallv.h
+++ src/bale_classic/convey/alltoallv.h
@@ -1,3 +1,12 @@
+// Copyright (c) 2020, Institute for Defense Analyses
+// 4850 Mark Center Drive, Alexandria, VA 22311-1882; 703-845-2500
+//
+// All rights reserved.
+//
+// This file is part of the conveyor package. For license information,
+// see the LICENSE file in the top level directory of the distribution.
+
+
 #ifndef CONVEY_ALLTOALLV_H
 #define CONVEY_ALLTOALLV_H
 
--- src/bale_classic/convey/biconvey.c
+++ src/bale_classic/convey/biconvey.c
@@ -1,3 +1,12 @@
+// Copyright (c) 2020, Institute for Defense Analyses
+// 4850 Mark Center Drive, Alexandria, VA 22311-1882; 703-845-2500
+//
+// All rights reserved.
+//
+// This file is part of the conveyor package. For license information,
+// see the LICENSE file in the top level directory of the distribution.
+
+
 #include "biconvey_impl.h"
 #include "private.h"
 #define PANIC(ERR) biconvey_panic(self, __func__, ERR)
